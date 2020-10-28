41c4dc7adb4a36c3d793693b963e71aa402d2104
Romain <romain.grimault@epsi.fr>
2020-10-28 14:06:38 +0100

    tp 4

---
+++ MAVEN/src/main/java/utils/ConversionNombre.java
@@ -0,0 +1,15 @@
+package utils;
+
+public class ConversionNombre {
+    public static void main(String[] args) {
+        String var_1 = "1";
+       int valeur_int= Integer.parseInt(var_1);
+        System.out.println(valeur_int);
+       int a=5;
+       int b=15;
+        System.out.println("Nombre max entre a et b");
+       /* pas trouver
+    int ret=   Integer.compare(a,b);
+        System.out.println(ret);*/
+    }
+}
